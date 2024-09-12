ef64f914ce5bd9d23e4055bdfe6714a305415f88
CodyKoInABox <codykoinabox@gmail.com>
2024-09-11 22:11:29 -0300

    renaming files

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,7 @@
 DO $$ 
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'bonsite_db') THEN
-        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE bonsite_db');
-    END IF;
-END $$;
-
-\c bonsite_db
-
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'categories') THEN
-        CREATE TABLE categories (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(50) NOT NULL
-        );
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'categoria_enum') THEN
+        CREATE TYPE categoria_enum AS ENUM ('Frutíferas', 'Floríferas', 'Perenes', 'Caducifólias');
     END IF;
 END $$;
 
@@ -22,41 +10,44 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'bonsais') THEN
         CREATE TABLE bonsais (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            category_id INT REFERENCES categories(id),
-            details JSONB
+            nome TEXT NOT NULL,
+            descricao TEXT,
+            categoria categoria_enum NOT NULL,
+            sol SMALLINT CHECK (sol BETWEEN 1 AND 5),
+            agua SMALLINT CHECK (agua BETWEEN 1 AND 5),
+            tamanho SMALLINT CHECK (tamanho BETWEEN 1 AND 5),
+            poda SMALLINT CHECK (poda BETWEEN 1 AND 5),
+            solo SMALLINT CHECK (solo BETWEEN 1 AND 5),
+            delicadeza SMALLINT CHECK (delicadeza BETWEEN 1 AND 5),
+            preco NUMERIC(6,2) NOT NULL
         );
     END IF;
 END $$;
 
-DO $$
+DO $$ 
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Frutíferas') THEN
-        INSERT INTO categories (name) VALUES ('Frutíferas');
-    END IF;
-    IF NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Floríferas') THEN
-        INSERT INTO categories (name) VALUES ('Floríferas');
-    END IF;
-    IF NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Perenes') THEN
-        INSERT INTO categories (name) VALUES ('Perenes');
-    END IF;
-    IF NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Caducifólias') THEN
-        INSERT INTO categories (name) VALUES ('Caducifólias');
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'pedidos') THEN
+        CREATE TABLE pedidos (
+            id SERIAL PRIMARY KEY,
+            preco_total NUMERIC(6, 2)
+        );
     END IF;
 END $$;
 
-DO $$
+DO $$ 
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM bonsais WHERE name = 'Bonsai de Jabuticaba') THEN
-        INSERT INTO bonsais (name, category_id, details) VALUES 
-        ('Bonsai de Jabuticaba', 1, 
-        '{
-            "sun_exposure": "Sol pleno",
-            "watering": "Moderada",
-            "size": "Pequeno",
-            "pruning": "Regular",
-            "fertilization": "Mensal",
-            "delicacy": "Alta"
-        }');
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'pedido_bonsais') THEN
+        CREATE TABLE pedido_bonsais (
+            pedido_id INT NOT NULL,
+            bonsai_id INT NOT NULL,
+            quantidade INT NOT NULL CHECK (quantidade > 0),
+            PRIMARY KEY (pedido_id, bonsai_id),
+            FOREIGN KEY (pedido_id) REFERENCES pedidos (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+            FOREIGN KEY (bonsai_id) REFERENCES bonsais (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+        );
     END IF;
 END $$;
